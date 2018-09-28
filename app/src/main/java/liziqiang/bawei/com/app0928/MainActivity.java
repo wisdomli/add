0d9183b184bc2e43d6b5c68fed786dfa3e44f7c0
wisdomli <43641891+wisdomli@users.noreply.github.com>
2018-09-28 15:22:41 +0800

    更新1
    add

--- app/src/main/java/liziqiang/bawei/com/app0928/MainActivity.java
+++ app/src/main/java/liziqiang/bawei/com/app0928/MainActivity.java
@@ -2,7 +2,7 @@ package liziqiang.bawei.com.app0928;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+// base a
 public class MainActivity extends AppCompatActivity {
 
     @Override
