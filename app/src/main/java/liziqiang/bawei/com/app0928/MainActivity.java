71aae5a78455d5cde125783d67a70522d89ee315
wisdomli <43641891+wisdomli@users.noreply.github.com>
2018-09-28 14:52:08 +0800

    addd

---
+++ app/src/main/java/liziqiang/bawei/com/app0928/MainActivity.java
@@ -0,0 +1,13 @@
+package liziqiang.bawei.com.app0928;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
